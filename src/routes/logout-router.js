645e825f91ef3d77f55afdc9450899bf034cb52e
sammimagg <sammimagg@gmail.com>
2023-02-23 19:32:40 +0000

    logout v3

--- src/routes/logout-router.js
+++ src/routes/logout-router.js
@@ -2,17 +2,9 @@ import express from 'express'
 export const logoutRouter = express.Router();
 
 logoutRouter.get('/', (req, res, next) => {
-    // Log out the user
-    req.logout();
-  
-    // Destroy the session
-    req.session.destroy((err) => {
-      if (err) {
-        return next(err);
-      }
-  
-      // Redirect to the home page
-      res.redirect('/');
-    });
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
   });
   
